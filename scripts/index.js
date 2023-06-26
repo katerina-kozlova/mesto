1731564dbdea1ba77b9e0ed7b2af2508f7e5a13e
Kozlova Katerina <kate.rina.kozlova@yandex.ru>
2023-06-26 19:20:09 +0600

    что-то пытаюсь сделать

--- scripts/index.js
+++ scripts/index.js
@@ -14,56 +14,114 @@ let linkInput = formElement.querySelector("#link");
 let cardsTitle = document.querySelector(".cards__title");
 let cardsImage = document.querySelector(".cards__image");
 
-// Функция, которая открывает необходимый попап в зависимости от того, на какую кнопку пользоваться кликнул
-function openPopup() {
-    let index = Array.from(openPopupButtons).indexOf(this);
-    popupsElement[index].classList.add("popup_opened");
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileDescription.textContent;
-}
-// Перебираем из массива кнопки открытия попапа и подключаем функцию, описанную выше
-openPopupButtons.forEach((button) => {
-    button.addEventListener('click', openPopup);
-    })
+const initialCards = [
+  {
+    name: 'Алтай',
+    link: 'https://images.unsplash.com/photo-1643281237857-5f14c2b9f3ba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'
+  },
+  {
+    name: 'Алтай',
+    link: 'https://images.unsplash.com/photo-1635499829006-f18084159cd7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=388&q=80'
+  },
+  {
+    name: 'Озеро Рица',
+    link: 'https://images.unsplash.com/photo-1631170706315-707ecc9f158b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=436&q=80'
+  },
+  {
+    name: 'Сиамский залив',
+    link: 'https://images.unsplash.com/photo-1596463621264-26fa44b7a2bf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1332&q=80'
+  },
+  {
+    name: 'Чёрное море',
+    link: 'https://images.unsplash.com/photo-1601033089147-f9d34a7629ff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80'
+  },
+  {
+    name: 'Река Катунь',
+    link: 'https://images.unsplash.com/photo-1564324738191-7f91304232eb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'
+  }
+];
 
-function closePopup() {
-    let index = Array.from(closePopupButtons).indexOf(this);
-    popupsElement[index].classList.remove("popup_opened");
-}
-closePopupButtons.forEach((button) => {
-    button.addEventListener('click', closePopup);
-    })
+const templateElement = document.querySelector('.card-template');
+const cardsContainer = document.querySelector('.cards');
 
-function savePopup() {
-    let index = Array.from(savePopupButtons).indexOf(this);
-    popupsElement[index].classList.remove("popup_opened");
+const createCard = ({ name, link }) => {
+  const clone = templateElement.content.cloneNode(true);
+  const cardsElement = clone.querySelector('.cards__element'); // Клонируем содержимое шаблона, чтобы добавить новую карточку
+  cardsElement.querySelector('.cards__image').src = link;
+  cardsElement.querySelector('.cards__title').textContent = name;
+  return cardsElement;
 }
-savePopupButtons.forEach((button) => {
-    button.addEventListener('click', savePopup);
-    })
+
+
+initialCards.forEach((item) => {
+  const cardElement = createCard(item);
+  cardsContainer.append(cardElement(item));
+});
 
 // Обработчик «отправки» формы редактирования профиля
 function handleFormSubmit(evt) {
-    evt.preventDefault();
-    // Эта строчка отменяет стандартную отправку формы.
-    // Так мы можем определить свою логику отправки.
-    profileName.textContent = nameInput.value;
-    profileDescription.textContent = jobInput.value;
-    // Получаем значение полей jobInput и nameInput из свойства value,
-    // Выберираем элементы, куда должны быть вставлены значения полей
-    // Вставляем новые значения с помощью textContent
-    closePopup();
+  evt.preventDefault();
+  const name = titleInput.value;
+  const link = linkInput.value;
+  const cardHtml = createCard({ name, link });
+  cardsContainer.insertAdjacentHTML('beforeend', cardHtml);
+  closePopup();
 }
 
-// Обработчик «отправки» формы сохдания карточки
-//function handleFormSubmit(evt) {
-    //evt.preventDefault();
-    //cardsTitle.textContent = titleInput.value;
-    //cardsImage.textContent = linkInput.value;
-    //closePopup();
-//}
+
+formElement.addEventListener("submit", handleFormSubmit);
+
+//const cardsTemplate = document.querySelector('#cards-template').content; // Находим тег temlate для карточки в DOM и обращаемся к его свойству через content
+//const cardsElement = cardsTemplate.querySelector('.cards__element').cloneNode(true); // Клонируем содержимое шаблона, чтобы добавить новую карточку
+//const container = document.querySelector('.elements');
+//initialCards.forEach((place) => {
+  
+  //cardsElement.querySelector('.cards__image').src = place.link;
+  //cardsElement.querySelector('.cards__title').textContent = place.name;
+  //container.append(cardsElement);
+//});
+
+// Функция, которая открывает необходимый попап в зависимости от того, на какую кнопку пользоваться кликнул
+function openPopup() {
+  let index = Array.from(openPopupButtons).indexOf(this);
+  popupsElement[index].classList.add("popup_opened");
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileDescription.textContent;
+}
+// Перебираем из массива кнопки открытия попапа и подключаем функцию, описанную выше
+openPopupButtons.forEach((button) => {
+  button.addEventListener('click', openPopup);
+  })
+
+function closePopup() {
+  let index = Array.from(closePopupButtons).indexOf(this);
+  popupsElement[index].classList.remove("popup_opened");
+}
+closePopupButtons.forEach((button) => {
+  button.addEventListener('click', closePopup);
+  })
+
+function savePopup() {
+  let index = Array.from(savePopupButtons).indexOf(this);
+  popupsElement[index].classList.remove("popup_opened");
+}
+savePopupButtons.forEach((button) => {
+  button.addEventListener('click', savePopup);
+  })
+
+// Обработчик «отправки» формы редактирования профиля
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  // Эта строчка отменяет стандартную отправку формы.
+  // Так мы можем определить свою логику отправки.
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+  // Получаем значение полей jobInput и nameInput из свойства value,
+  // Выберираем элементы, куда должны быть вставлены значения полей
+  // Вставляем новые значения с помощью textContent
+  closePopup();
+}
 
 formElement.addEventListener("submit", handleFormSubmit);
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-
